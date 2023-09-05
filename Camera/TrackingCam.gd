291a4cb42f5b86db4d6810c051bf8a859989c8bb
Xiangyu <chenxy327@gmail.com>
2023-09-05 02:31:08 -0400

    fix tracking cam re-tracking when target is clamped on trigger axis

--- Camera/TrackingCam.gd
+++ Camera/TrackingCam.gd
@@ -4,11 +4,12 @@ class_name TrackingCam
 @onready var game:Node2D = $"/root/Game";
 @onready var level:Node2D = game.current_level;
 
-@export var max_rx:float = 0.28; #ratio between dx and level width to make camera move
+@export var max_rx:float = 0.3125; #ratio between dx and level width to make camera move
 @export var max_ry:float = 0.25;
 
 var track_pos:Vector2;
 var track_dir:Vector2i;
+var clamped_track_dir:Vector2i; #track_dir but only if target is clamped
 
 var tracking:bool = true;
 var max_dx:float;
@@ -52,36 +53,53 @@ func _process(_delta):
 		track_pos = avg_player_pos();
 		
 		#figure out whether to move
+		#if tracking towards clamped target, don't restart track in that direction
 		var dx = track_pos.x - position.x;
 		var dy = track_pos.y - position.y;
 		var track:bool = false;
 		track_dir = Vector2i.ZERO;
-		if (dx >= max_dx and position.x != max_pos.x):
+		if (dx >= max_dx and !GV.is_approx_equal(position.x, max_pos.x, 0.005) and clamped_track_dir.x != 1):
 			track_dir.x = 1;
 			track = true;
-		if (dx <= -max_dx and position.x != min_pos.x):
+		elif (dx <= -max_dx and !GV.is_approx_equal(position.x, min_pos.x, 0.005) and clamped_track_dir.x != -1):
 			track_dir.x = -1;
 			track = true;
-		if (dy >= max_dy and position.y != max_pos.y):
+		if (dy >= max_dy and !GV.is_approx_equal(position.y, max_pos.y, 0.005) and clamped_track_dir.y != 1):
 			track_dir.y = 1;
 			track = true;
-		if (dy <= -max_dy and position.y != min_pos.y):
+		elif (dy <= -max_dy and !GV.is_approx_equal(position.y, min_pos.y, 0.005) and clamped_track_dir.y != -1):
 			track_dir.y = -1;
 			track = true;
 		
 		if track:
-			#slide to current track_pos
-			tween = create_tween();
-			tween.set_ease(Tween.EASE_OUT);
-			tween.finished.connect(_on_tween_transitioned);
-			tween.set_process_mode(Tween.TWEEN_PROCESS_IDLE);
-			
 			var track_rx:float = GV.TRACKING_CAM_LEAD_RATIO if track_dir.x else GV.TRACKING_CAM_SLACK_RATIO;
 			var track_ry:float = GV.TRACKING_CAM_LEAD_RATIO if track_dir.y else GV.TRACKING_CAM_SLACK_RATIO;
 			target.x = position.x + track_rx * (track_pos.x - position.x);
 			target.y = position.y + track_ry * (track_pos.y - position.y);
-			target = target.clamp(min_pos, max_pos);
+			
+			#target = target.clamp(min_pos, max_pos);
+			#clamp target, set clamped_track_dir
+			clamped_track_dir = Vector2i.ZERO;
+			if target.x <= min_pos.x:
+				target.x = min_pos.x;
+				clamped_track_dir.x = -1;
+			elif target.x >= max_pos.x:
+				target.x = max_pos.x;
+				clamped_track_dir.x = 1;
+			if target.y <= min_pos.y:
+				target.y = min_pos.y;
+				clamped_track_dir.y = -1;
+			elif target.y >= max_pos.y:
+				target.y = max_pos.y;
+				clamped_track_dir.y = 1;
+			
+			tween = create_tween();
+			tween.set_ease(Tween.EASE_OUT);
+			tween.finished.connect(_on_tween_transitioned);
+			tween.set_process_mode(Tween.TWEEN_PROCESS_IDLE);
 			tween.tween_property(self, "position", target, GV.TRACKING_CAM_TRANSITION_TIME).set_trans(Tween.TRANS_QUINT);
+			#transitioned = false; #comment this line for continuous tracking
 
 func _on_tween_transitioned():
 	transitioned = true;
+	clamped_track_dir = Vector2i.ZERO;
--- Global Variables.gd
+++ Global Variables.gd
@@ -73,7 +73,7 @@ const LEVEL_NAME_DISPLAY_TIME:float = 3;
 const LEVEL_NAME_FADE_OUT_TIME:float = 1.2;
 
 const TRACKING_CAM_LEAD_RATIO:float = 1.35; #target = pos + ratio * (track_pos - pos)
-const TRACKING_CAM_SLACK_RATIO:float = 0.15; #ratio applied to slack (tracking movement along the non-trigger axis)
+const TRACKING_CAM_SLACK_RATIO:float = 0.15; #0.25; #ratio applied to slack (tracking movement along the non-trigger axis)
 const TRACKING_CAM_TRANSITION_TIME:float = 1.28;
 const PLAYER_SPAWN_INVINCIBILITY_TIME:float = 0.25;
 
@@ -235,3 +235,8 @@ func combinations_dp(n, k) -> int:
 
 func world_to_pos_t(pos:Vector2) -> Vector2i:
 	return Vector2i(pos/TILE_WIDTH);
+
+func is_approx_equal(a:float, b:float, tolerance:float) -> bool:
+	if abs(a - b) <= tolerance:
+		return true;
+	return false;
