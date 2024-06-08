6ea5b22838228ac59b9565a83d00a5710b04480b
Xiangyu Chen <chenxy327@gmail.com>
2024-06-08 14:44:56 +0800

    some unsubstantial pathfinding

--- Pathfinder/Pathfinder.gd
+++ Pathfinder/Pathfinder.gd
@@ -1,9 +1,27 @@
 extends Pathfinder
 
+var cells:TileMap;
+
 
 func _ready():
-	#set_gv(GV);
-	set_max_depth(500);
-	set_tile_push_limit(GV.abilities["tile_push_limit"]);
-	set_tile_pow_max(GV.TILE_POW_MAX);
-	set_is_player(true);
+	cells = get_parent().get_node("Cells");
+#	#set_gv(GV);
+#	set_max_depth(500);
+#	set_tile_push_limit(GV.abilities["tile_push_limit"]);
+#	set_tile_pow_max(GV.TILE_POW_MAX);
+#	set_is_player(true);
+
+func pathfind(search_id, min, max, start, end):
+	match search_id:
+		GV.SearchId.DIJKSTRA:
+			return pathfind_dijkstra(min, max, start, end);
+		GV.SearchId.ASTAR:
+			pass;
+		GV.SearchId.IDASTAR:
+			pass;
+		_:
+			pass;
+
+func pathfind_dijkstra(min, max, start, end):
+	var wavefront:Array;
+	
